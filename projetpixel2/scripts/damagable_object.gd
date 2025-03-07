2f6aacf74ab1393ea837c10f00d17abad807b2d2
Maxou <mm.maxencemaire@gmail.com>
2025-03-07 22:49:35 +0100

    created damagable object "component"

---
+++ projetpixel2/scripts/damagable_object.gd
@@ -0,0 +1,15 @@
+extends Node
+class_name DamagableObject
+
+signal hit(damage_amount : int, new_health : int)
+signal death
+
+var health : int = 100
+
+func damage(damage_amount : int) -> void:
+	health -= damage_amount
+	if health < 0:
+		health = 0
+	emit_signal("hit", damage_amount, health)
+	if health == 0:
+		emit_signal("death")
