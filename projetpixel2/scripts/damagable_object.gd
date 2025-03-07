565b298a5c5c33e800277fa2766681faee9e0a08
Maxou <mm.maxencemaire@gmail.com>
2025-03-07 23:25:59 +0100

    wip damage system

--- projetpixel2/scripts/damagable_object.gd
+++ projetpixel2/scripts/damagable_object.gd
@@ -5,9 +5,47 @@ class_name DamagableObject
 signal hit(damage_amount : int, new_health : int)
 signal death
 
-@export var health : int = 100
+enum DamagableTypes {Neutral, Energy, Structure}
+enum DamagingTypes {Neutral, TrueDamage, Energy, Fire}
 
-func damage(damage_amount : int) -> void:
+static var damage_table : Dictionary = {
+	DamagingTypes.Neutral : {
+		DamagableTypes.Neutral : 1.0,
+		DamagableTypes.Energy : 0.8,
+		DamagableTypes.Structure : 1.0,
+	},
+	DamagingTypes.TrueDamage : {
+		DamagableTypes.Neutral : 1.0,
+		DamagableTypes.Energy : 1.0,
+		DamagableTypes.Structure : 1.0,
+	},
+	DamagingTypes.Energy : {
+		DamagableTypes.Neutral : 1.0,
+		DamagableTypes.Energy : 0.5,
+		DamagableTypes.Structure : 1.5,
+	},
+	DamagingTypes.Fire : {
+		DamagableTypes.Neutral : 1.0,
+		DamagableTypes.Energy : 1.5,
+		DamagableTypes.Structure : 0.75,
+	},
+}
+
+@export var health : int = 100
+@export var type := DamagableTypes.Neutral
+@export var defense := 1.0
+
+static func compute_type_coeff(damaging_type : DamagingTypes, damaged_type : DamagableTypes) -> float:
+	return damage_table[damaging_type][damaged_type]
+
+static func compute_damage(damage_amount : int, damage_type : DamagingTypes, 
+				damaged_type : DamagableTypes, defense : float) -> int:
+	var type_coeff : float = compute_type_coeff(damage_type, damaged_type)
+	
+	return int(damage_amount * type_coeff / defense)
+
+func damage(damage_amount : int, damage_type : DamagingTypes) -> void:
+	var type_coeff : float = compute_type_coeff(damage_type, type)
 	health -= damage_amount
 	if health < 0:
 		health = 0
--- projetpixel2/scripts/damaging_object.gd
+++ projetpixel2/scripts/damaging_object.gd
@@ -1,5 +1,18 @@
 extends Node
 class_name DamagingObject
 
-static func create_attack() -> DamagingObject:
+static func create_attack(dmg_amount := 10, dmg_type := DamageTypes.Neutral) -> DamagingObject:
 	return DamagingObject.new()
+
+enum DamageTypes {Neutral, TrueDamage, Energy, Fire}
+
+var damage_amount : int
+var damage_type : DamageTypes = DamageTypes.Energy
+
+func set_type(new_type : DamageTypes) -> DamagingObject:
+	damage_type = new_type
+	return self
+
+func set_amount(new_amount : int) -> DamagingObject:
+	damage_amount = new_amount
+	return self
