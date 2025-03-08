fa730a21e7a001d3003c8c309ff8ae49998794a2
Maxou <mm.maxencemaire@gmail.com>
2025-03-08 01:23:33 +0100

    tower works, kills enemies (wow, such death)

--- projetpixel2/scripts/tower_base.gd
+++ projetpixel2/scripts/tower_base.gd
@@ -2,16 +2,28 @@ extends Node3D
 class_name TowerBase
 
 
+const PROJECTILE_RES := preload("res://scenes/spaceship/towers/projectiles/projectile_base.tscn")
+
+var shoot_delay := 0.25
+var time_before_shoot := 0.0
 var focused_enemies : Array[BaseEnemy] = []
 
 
 func shoot(enemy : BaseEnemy) -> void:
+	time_before_shoot = shoot_delay
 	look_at(enemy.position)
-	
+	var projectile := PROJECTILE_RES.instantiate()
+	GV.world.add_child(projectile)
+	projectile.position = $ProjectileSpawnPos.global_position
+	projectile.direction = projectile.position.direction_to(enemy.position)
 
 func _process(delta: float) -> void:
-	if len(focused_enemies) > 0:
-		shoot(focused_enemies[0])
+	time_before_shoot -= delta 
+	if len(focused_enemies) > 0 and time_before_shoot < 0:
+		if not is_instance_valid(focused_enemies[0]):
+			focused_enemies.pop_front()
+		else:
+			shoot(focused_enemies[0])
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is BaseEnemy:
