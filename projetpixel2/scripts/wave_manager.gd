407a8af1c0f1447c6cb2d3bfdcca8ee43e55bea4
Maxou <mm.maxencemaire@gmail.com>
2025-03-12 20:10:06 +0100

    exposed wave number of enemies

--- projetpixel2/scripts/wave_manager.gd
+++ projetpixel2/scripts/wave_manager.gd
@@ -4,9 +4,10 @@ class_name WaveManager
 @export var current_wave_number : int = 0
 @export var current_wave_difficulty : float = 1.0
 @export var current_wave_enemies : Array[BaseEnemy] = []
+@export var current_wave_number_of_enemies : int = 25
 
 func _ready() -> void:
 	await(get_tree().process_frame)
 	for spawner : EnemySpawner in GV.spawners:
-		spawner.spawn_wave(50 / len(GV.spawners))
+		spawner.spawn_wave(current_wave_number_of_enemies / len(GV.spawners))
 	
