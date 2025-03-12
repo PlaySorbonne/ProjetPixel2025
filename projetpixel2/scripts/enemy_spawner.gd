d84820c55c16659e8d69f829e7362e02c234d284
Maxou <mm.maxencemaire@gmail.com>
2025-03-12 19:57:22 +0100

    added radius to enemy spawner

--- projetpixel2/scripts/enemy_spawner.gd
+++ projetpixel2/scripts/enemy_spawner.gd
@@ -1,14 +1,21 @@
+@tool
 extends Node3D
 class_name EnemySpawner
 
-func _ready() -> void:
-	await get_tree().process_frame
-	spawn_wave(25)
+@export var spawner_radius := 1.0:
+	set(new_val):
+		if new_val > 0.0:
+			spawner_radius = new_val
+			$CSGCylinder3D.radius = spawner_radius
 
 func spawn_enemy() -> void:
 	var enemy : BaseEnemy = preload("res://scenes/world/enemies/base_enemy.tscn").instantiate()
 	GV.world.add_child(enemy)
-	enemy.position = self.position
+	enemy.position = self.position + Vector3(
+		randf_range(-spawner_radius*100.0, spawner_radius*100.0),
+		0.0,
+		randf_range(-spawner_radius*100.0, spawner_radius*100.0)
+	)
 	print("new enemy!")
 
 func spawn_wave(nb_enemies : int) -> void:
