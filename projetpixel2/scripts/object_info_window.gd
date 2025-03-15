8effdfdbb7bea8231d98c8febac1de9560df95fe
Maxou <mm.maxencemaire@gmail.com>
2025-03-15 17:57:57 +0100

    added hide/show widget animation

--- projetpixel2/scripts/object_info_window.gd
+++ projetpixel2/scripts/object_info_window.gd
@@ -2,17 +2,29 @@ extends Control
 class_name ObjectInfoWindow
 
 
+var is_window_displayed := false
 var selected_object : Node3D = null
 var selected_clickable : ClickableObject = null
 var obj_damageable : DamageableObject = null
 
 func _ready() -> void:
 	$Label.text = ""
+	scale = Vector2.ZERO
+
+func display_window() -> void:
+	is_window_displayed = true
+	$AnimationPlayer.play("show")
+
+func hide_window() -> void:
+	is_window_displayed = false
+	$AnimationPlayer.play("hide")
 
 func select_object(new_obj : Node3D) -> void:
 	if new_obj == null:
 		deselect_object()
 		return
+	if not is_window_displayed:
+		display_window()
 	selected_object = new_obj
 	selected_clickable = selected_object.clickable
 	if "damageable" in selected_object:
@@ -25,6 +37,8 @@ func select_object(new_obj : Node3D) -> void:
 func deselect_object() -> void:
 	selected_object = null
 	$Label.text = "No selected object."
+	if is_window_displayed:
+		hide_window()
 
 func selected_obj_hit(_dmg : int, _new_health : int) -> void:
 	update_object_infos()
