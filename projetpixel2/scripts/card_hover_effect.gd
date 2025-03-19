7c1583af55f64b45576eccb6ace63f744e535903
Maxou <mm.maxencemaire@gmail.com>
2025-03-19 08:33:38 +0100

    fix rotation & cleanup code

--- projetpixel2/scripts/card_hover_effect.gd
+++ projetpixel2/scripts/card_hover_effect.gd
@@ -1,5 +1,9 @@
 extends Node
 
+const ROTATION_LIMIT := 75.0
+const FOLLOW_SPEED := 20.0
+const ROTATION_RETURN_SPEED: float = 10.0 
+
 @export var normal : Marker2D
 @export var enabled : bool = true
 @export var chip : Sprite2D 
@@ -11,14 +15,7 @@ var blaa := 0
 var is_mouse_over := false
 var is_grabbed := false
 var mouse_grab_offset : Vector2
-var time_since_movement_start := 0.0
-var at_rest := true
-
-var movement_start_pos : Vector2
-
-var follow_speed := 20.0
-var max_rotation: float = 0.15  
-var rotation_return_speed: float = 5.0  
+var at_rest := true 
 var last_position := Vector2.ZERO  
 
 @onready var card_object : CardObject = get_parent()
@@ -33,6 +30,8 @@ func _process(delta: float) -> void:
 		process_card_movement(delta)
 
 func process_card_movement(delta : float) -> void:
+	if at_rest:
+		return
 	var target_pos = card_object.get_global_mouse_position()
 	if is_grabbed:
 		target_pos = card_object.get_global_mouse_position() - mouse_grab_offset
@@ -43,53 +42,19 @@ func process_card_movement(delta : float) -> void:
 	card_object.global_position = lerp(
 		card_object.global_position,
 		target_pos,
-		follow_speed * delta
+		FOLLOW_SPEED * delta
 	)
-	
 	
 	# rotation
-	# Calculate velocity based on position difference
-	var velocity = card_object.global_position - last_position
-	last_position = card_object.global_position  # Update for next frame
-
-	var speed_factor = min(velocity.length() / 10.0, 10.0) # Normalize speed [0,1]
-	var target_rotation = max(-1.57, min(velocity.x * max_rotation, 1.57))  # Tilt based on horizontal movement
-
-	target_rotation = (target_pos - last_position).angle() * -1.0
-	chip.rotation = target_rotation
-	#chip.rotation = lerp(
-		#chip.rotation,
-		#target_rotation * speed_factor,  # Blend tilt based on speed
-		#rotation_return_speed * delta
-	#)
-	
-
-func process_old(delta : float) -> void:
-	if at_rest:
-		return
-	var target_position : Vector2
-	if is_grabbed:
-		target_position = card_object.get_global_mouse_position() - mouse_grab_offset
-	else:
-		target_position = initial_position
-	# move
-	card_object.global_position = lerp(
-		card_object.global_position,
-		target_position,
-		follow_speed * delta
+	var velocity = target_pos - last_position
+	last_position = card_object.global_position
+	var target_rotation = max(min( (velocity.x) * velocity.length() / 1000.0,
+				ROTATION_LIMIT ), -ROTATION_LIMIT )
+	chip.rotation_degrees = lerp(
+		chip.rotation_degrees,
+		target_rotation,
+		ROTATION_RETURN_SPEED * delta
 	)
-	# rotate
-	#var direction = (target_position - card_object.global_position).normalized()
-	#var target_rotation = direction.angle()
-	
-	
-	#var direction = (target_position - card_object.global_position)
-	#if direction.length() > rotation_threshold:
-		#var target_rotation = direction.angle()
-		#card_object.rotation = lerp_angle(
-			#card_object.rotation, 
-			#target_rotation, 
-			#rotation_speed * delta)
 
 func process_shader_effect() -> void:
 	var material := chip.material
@@ -111,9 +76,7 @@ func process_shader_effect() -> void:
 	suit.material = material
 
 func start_movement() -> void:
-	time_since_movement_start = 0.0
 	at_rest = false
-	movement_start_pos = card_object.global_position
 
 # card_object signals processing 
 func _on_card_object_mouse_entered() -> void:
