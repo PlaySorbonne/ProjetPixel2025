765baa0464d50feab1d14756d7d740d4398cd6cf
Maxou <mm.maxencemaire@gmail.com>
2025-03-27 18:30:23 +0100

    maybe use global script with all card functions, wip

--- projetpixel2/scripts/card_object.gd
+++ projetpixel2/scripts/card_object.gd
@@ -7,3 +7,6 @@ var card_target := CardTargets.Tower     # the target of the effect
 var card_signal : String = "tower_fired" # the target's signal that triggers the effect
 var card_trigger_condition : Callable    # a boolean function to make sure the effect triggers
 var card_effect_code : Callable          # the method to call when the effect triggers
+
+
+# question: is it better to use Expression or Callable in this case? 
