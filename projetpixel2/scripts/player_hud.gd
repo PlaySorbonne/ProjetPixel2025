f6dd25fc4ab09530a1360db840478436275d6feb
Maxou <mm.maxencemaire@gmail.com>
2025-03-29 17:45:31 +0100

    wip HUD

--- projetpixel2/scripts/player_hud.gd
+++ projetpixel2/scripts/player_hud.gd
@@ -5,7 +5,6 @@ class_name PlayerHud
 var in_combo := false
 
 # components
-@onready var create_towers : CreateTowerWindow = $hud_control/CreateTowerWindow
 @onready var mouse_cursor_hint : MouseCursorHint = $MouseCursorHint
 @onready var tower_spawner : TowerSpawner = $TowerSpawner
 @onready var mouse_3d_interaction : Mouse3dInteraction = $Mouse3dInteraction
@@ -31,12 +30,12 @@ func _on_mouse_3d_interaction_select_new_object(object: Node3D) -> void:
 	if object == null:
 		mouse_cursor_hint.add_mouse_hint()
 		#object_info.deselect_object()
-		create_towers.show_window()
+		#create_towers.show_window()
 	else:
 		print("object selected: " + str(object))
 		#object_info.select_object(object)
 		mouse_cursor_hint.hide_mouse_hint()
-		create_towers.hide_window()
+		#create_towers.hide_window()
 
 func _on_create_tower_window_tower_placed() -> void:
 	tower_spawner.spawn_tower(
