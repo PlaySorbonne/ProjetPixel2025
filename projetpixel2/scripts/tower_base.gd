4afb5b8b07c564f5b3aa46d84a1fbde59716141f
Maxou <mm.maxencemaire@gmail.com>
2025-03-29 12:04:20 +0100

    wip & optimization of base tower behavior

--- projetpixel2/scripts/tower_base.gd
+++ projetpixel2/scripts/tower_base.gd
@@ -9,12 +9,18 @@ signal tower_switched_mode
 signal tower_collected_xp
 signal tower_upgraded
 
+@export_group("Tower Stats")
+@export var projectile_type : PackedScene = PROJECTILE_RES
+@export var number_of_projectiles := 1
+@export var fire_rate := 1.0
+
+@export_group("Cards")
 @export var cards_tower : Array[CardObject] = []
 @export var cards_projectile : Array[CardObject] = []
 @export var cards_enemy : Array[CardObject] = []
 
-@export var shoot_delay := 0.25
-var time_before_shoot := 0.0
+@onready var shoot_delay := 1.0 / fire_rate
+var can_shoot := true
 var focused_enemies : Array[BaseEnemy] = []
 var cards : Dictionary[String, Array] = {}
 
@@ -32,21 +38,32 @@ func shoot(enemy : BaseEnemy) -> void:
 	# to "tower_fired" and each card can modify prepared_projectile before actually shooting?
 	#for card : CardObject in cards["tower_fired"]:
 		#pass
-	time_before_shoot = shoot_delay
+	can_shoot = false
 	look_at(enemy.position)
 	var projectile := PROJECTILE_RES.instantiate()
 	GV.world.add_child(projectile)
 	projectile.position = $ProjectileSpawnPos.global_position
 	projectile.direction = projectile.position.direction_to(enemy.position)
-
-func _process(delta: float) -> void:
-	time_before_shoot -= delta 
-	if len(focused_enemies) > 0 and time_before_shoot < 0:
-		if not is_instance_valid(focused_enemies[0]):
-			focused_enemies.pop_front()
-		else:
-			shoot(focused_enemies[0])
+	$TimerShoot.start(shoot_delay)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is BaseEnemy:
 		focused_enemies.append(body)
+		body.connect("enemy_killed", remove_focus_enemy)
+		if can_shoot:
+			shoot(body)
+
+func remove_focus_enemy(enemy : BaseEnemy) -> void:
+	focused_enemies.erase(enemy)
+
+func _on_timer_shoot_timeout() -> void:
+	can_shoot = true
+	if len(focused_enemies) > 0:
+		while len(focused_enemies) > 0:
+			var focused_enemy := len(focused_enemies) - 1
+			if not is_instance_valid(focused_enemies[focused_enemy]):
+				focused_enemies.pop_back()
+				focused_enemy -= 1
+			else:
+				shoot(focused_enemies[focused_enemy])
+				break
