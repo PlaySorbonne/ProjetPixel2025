d39cbd1948c41f49d2f9ab672114e3c83865352d
Maxou <mm.maxencemaire@gmail.com>
2025-04-01 21:28:07 +0200

    added new wave system & UI

--- projetpixel2/scripts/next_wave_button.gd
+++ projetpixel2/scripts/next_wave_button.gd
@@ -3,15 +3,27 @@ class_name NextWaveButton
 
 signal next_wave_triggered
 
+var wave : WaveManager.EnemyWave
+var is_next_wave := false
 var is_triggered := false
+
+func set_next_wave(is_next := true) -> void:
+	is_next_wave = is_next
+	$Button.disabled = not is_next
+
+func set_wave(w : WaveManager.EnemyWave) -> void:
+	wave = w
+	$Button.text = "Next wave (" + str(wave.wave_number_of_enemies) + ")"
+
+func remove_button() -> void:
+	is_triggered = true
+	queue_free()
 
 func trigger_wave() -> void:
 	if is_triggered:
 		return
 	is_triggered = true
 	emit_signal("next_wave_triggered")
-	queue_free()
-
 
 func _on_button_pressed() -> void:
 	trigger_wave()
--- projetpixel2/scripts/wave_manager.gd
+++ projetpixel2/scripts/wave_manager.gd
@@ -1,36 +1,69 @@
 extends Node
 class_name WaveManager
 
-@export var wave_number : int = 0
-@export var wave_difficulty : float = 1.0
-@export var wave_enemies : Array[BaseEnemy] = []
-@export var wave_number_of_enemies : int = 15
-@export var wave_duration := 30.0
+const NEXT_BUTTON_RES := preload("res://scenes/interface/gameplay_hud/next_wave_button.tscn")
 
+class EnemyWave:
+	var wave_number : int = 0
+	var wave_difficulty : float = 1.0
+	var wave_enemies : Array[BaseEnemy] = []
+	var wave_number_of_enemies : int = 15
+	var wave_duration := 30.0
+	var is_boss_wave := false
+
+var waves : Array[EnemyWave] = []
+var wave_buttons : Array[NextWaveButton] = []
+var current_wave_id : int = 0
+var max_wave := 0
+
+# components
+@onready var vbox_container := $CanvasLayer/VBoxContainer
 
 
 func _ready() -> void:
 	GV.wave_manager = self
 	await(get_tree().process_frame)
 	initialize_spawners()
-	spawn_next_wave()
+	for i : int in range(9):
+		var n_v := EnemyWave.new()
+		n_v.wave_number_of_enemies += randi_range(i, i*2)
+		n_v.wave_number = i+1
+		n_v.wave_difficulty = n_v.wave_number_of_enemies
+		n_v.wave_duration = 30.0
+		n_v.is_boss_wave = false
+		waves.append(n_v)
+		var button := add_wave_button(n_v)
+		if i==0:
+			button.set_next_wave()
+
+func _process(delta: float) -> void:
+	$CanvasLayer/VBoxContainer/Label.text = str(int($Timer.time_left))
+
+func add_wave_button(w : EnemyWave) -> NextWaveButton:
+	var button : NextWaveButton = NEXT_BUTTON_RES.instantiate()
+	button.set_wave(w)
+	vbox_container.add_child(button)
+	wave_buttons.append(button)
+	button.connect("next_wave_triggered", spawn_next_wave)
+	vbox_container.move_child(button, 0)
+	max_wave += 1
+	return button
 
 func initialize_spawners() -> void:
 	for spawner : EnemySpawner in GV.spawners:
 		spawner.wave_manager = self
 
 func spawn_next_wave() -> void:
+	if current_wave_id < max_wave:
+		wave_buttons[current_wave_id].remove_button()
+	var current_wave : EnemyWave = waves[current_wave_id]
 	for spawner : EnemySpawner in GV.spawners:
-		spawner.spawn_wave(wave_number_of_enemies / len(GV.spawners))
-	increment_wave_difficulty()
-	$Timer.start(wave_duration)
-
-func increment_wave_difficulty() -> void:
-	wave_number_of_enemies += 5
-
-func _on_button_pressed() -> void:
-	$Timer.stop()
-	spawn_next_wave()
+		spawner.spawn_wave(current_wave.wave_number_of_enemies / len(GV.spawners))
+	current_wave_id += 1
+	if current_wave_id < max_wave:
+		wave_buttons[current_wave_id].set_next_wave()
+		$Timer.start(waves[current_wave_id].wave_duration)
+	print_debug("TODO: GENERATE ANOTHER WAVE")
 
 func _on_timer_timeout() -> void:
 	spawn_next_wave()
