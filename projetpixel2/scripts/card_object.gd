9914653d6fba25ac3abbf157c343674aa05ec0c9
Maxou <mm.maxencemaire@gmail.com>
2025-04-08 15:26:33 +0200

    simplified card parsing code

--- projetpixel2/scripts/card_object.gd
+++ projetpixel2/scripts/card_object.gd
@@ -10,12 +10,35 @@ class Card:
 	var trigger_condition : Expression  # a boolean function to make sure the effect triggers
 	var effect : Array[Expression] # the method to call when the effect triggers
 	
+	func parse_from_csv(csv_line : PackedStringArray) -> void:
+		name = csv_line[0]
+		description = csv_line[1]
+		value = int(csv_line[2])
+		@warning_ignore("int_as_enum_without_cast")
+		rarity = int(csv_line[3])
+		trigger_signal = csv_line[4]
+		trigger_condition = Expression.new()
+		if csv_line[5] == "":
+			trigger_condition.parse("true")
+		else:
+			var err := trigger_condition.parse(csv_line[5])
+			if err != Error.OK:
+				print_debug("error parsing card: " + name + " -> bad condition [" + csv_line[5] + "]")
+		effect = []
+		for effect_line : String in csv_line[6].split("\n"):
+			var effect_expr := Expression.new()
+			var err := effect_expr.parse(effect_line)
+			if err != Error.OK:
+				print_debug("error parsing card: " + name + " -> bad effect [" + csv_line[6] + "]")
+			effect.append(effect_expr)
+	
 	func card_to_string() -> String:
 		var card_string = "card[name='"+name+"', description='"+description
 		card_string += "', value="+str(value)+", rarity="+str(rarity)+", trigger_signal='"
 		card_string += trigger_signal+"', trigger_condition='"+str(trigger_condition)
 		card_string += "', effect="+str(effect)+"']"
 		return card_string
+
 
 enum CardRarities {Common, Uncommon, Rare, Legendary, Secret}
 
@@ -33,31 +56,9 @@ static func load_cards_data() -> void:
 	var card_file := FileAccess.open(CARDS_FILE_PATH, FileAccess.READ)
 	card_file.get_csv_line() # remove the top line, which is just titles
 	while not card_file.eof_reached():
-		var card_valid := true
-		var csv_line : PackedStringArray = card_file.get_csv_line()
 		var new_card := Card.new()
-		new_card.name = csv_line[0]
-		new_card.description = csv_line[1]
-		new_card.value = int(csv_line[2])
-		@warning_ignore("int_as_enum_without_cast")
-		new_card.rarity = int(csv_line[3])
-		new_card.trigger_signal = csv_line[4]
-		new_card.trigger_condition = Expression.new()
-		if csv_line[5] == "":
-			new_card.trigger_condition.parse("true")
-		else:
-			var err := new_card.trigger_condition.parse(csv_line[5])
-			if err != Error.OK:
-				print_debug("error parsing card: " + new_card.name + " -> bad condition [" + csv_line[5] + "]")
-		new_card.effect = []
-		for effect_line : String in csv_line[6].split("\n"):
-			var effect_expr := Expression.new()
-			var err := effect_expr.parse(effect_line)
-			if err != Error.OK:
-				print_debug("error parsing card: " + new_card.name + " -> bad effect [" + csv_line[6] + "]")
-			new_card.effect.append(effect_expr)
-		if card_valid:
-			cards_data[new_card.name] = new_card
+		new_card.parse_from_csv(card_file.get_csv_line())
+		cards_data[new_card.name] = new_card
 	card_file.close()
 	print("parse complete, dictionary:")
 	for k : String in cards_data.keys():
