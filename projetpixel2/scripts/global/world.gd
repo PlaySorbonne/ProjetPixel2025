ee2be44bc08376a08fbfd672f4feaf304df4e5b0
Maxou <mm.maxencemaire@gmail.com>
2025-04-21 17:19:28 +0200

    Update world.gd

--- projetpixel2/scripts/global/world.gd
+++ projetpixel2/scripts/global/world.gd
@@ -6,12 +6,4 @@ func _ready() -> void:
 	RunData.reset_run_data()
 	GV.world = self
 	
-	
 	Card.load_cards_data()
-	while true:
-		var rend := 0.0
-		for i in range(1000):
-			rend += RunData.random_float(0.0, 1.0)
-		rend /= 1000.0
-		print("random average = " + str(rend))
-		await get_tree().create_timer(1.0).timeout
