c26ecf29fabb991ea623669ef9ee54bd4792a1ce
Maxou <mm.maxencemaire@gmail.com>
2025-05-01 18:58:32 +0200

    create auraBase Object

---
+++ projetpixel2/scripts/aura_base.gd
@@ -0,0 +1,10 @@
+extends Area3D
+class_name AuraBase
+
+static func spawn_aura(radius : float) -> AuraBase:
+	
+	return null
+
+static func spawn_aura_on_object(radius : float, parent : Node3D) -> AuraBase:
+	
+	return null
