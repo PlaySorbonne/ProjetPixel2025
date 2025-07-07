7dae02c9dadea04a06a621c43728e8a85880cce5
Maxou <mm.maxencemaire@gmail.com>
2025-07-07 15:55:09 +0200

    Update minimal_enemy.gd

--- projetpixel2/scenes/world/testing/minimal_enemy.gd
+++ projetpixel2/scenes/world/testing/minimal_enemy.gd
@@ -5,16 +5,24 @@ const SPEED := 25.0
 
 var velocity : Vector3
 var is_moving := false
+var recompute_dir_in_process := true
 
 func _ready() -> void:
 	await get_tree().create_timer(2.0).timeout
 	is_moving = true
-	velocity = SPEED * position.direction_to(GV.space_ship.position)
-	$Timer.start()
+	compute_velocity()
+	if not recompute_dir_in_process:
+		$Timer.start()
 
 func _process(delta: float) -> void:
 	if is_moving:
 		position += velocity * delta
+		if recompute_dir_in_process:
+			compute_velocity()
 
 func _on_timer_timeout() -> void:
+	compute_velocity()
+
+func compute_velocity() -> void:
 	velocity = SPEED * position.direction_to(GV.space_ship.position)
+	look_at(GV.space_ship.position)
