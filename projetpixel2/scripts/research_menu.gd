3e16115c34a668fee191459f6733a56fd9e52801
Maxou <mm.maxencemaire@gmail.com>
2025-07-20 22:03:28 +0200

    wip research menu

--- projetpixel2/scripts/research_menu.gd
+++ projetpixel2/scripts/research_menu.gd
@@ -1,2 +1,19 @@
 extends Submenu
 class_name ResearchMenu
+
+
+func _on_button_toggle_enemy_view_pressed() -> void:
+	$ColorRect/AnatomicDrawing.visible = not $ColorRect/AnatomicDrawing.visible
+
+
+func _on_button_wip_pressed() -> void:
+	if $ColorRectResearchSlots.visible:
+		$ColorRectResearchSlots.visible = false
+		$ResearchButton.visible = false
+		$EnemyStats.visible = true
+		$EnemyDescription.visible = true
+	else:
+		$ColorRectResearchSlots.visible = true
+		$ResearchButton.visible = true
+		$EnemyStats.visible = false
+		$EnemyDescription.visible = false
