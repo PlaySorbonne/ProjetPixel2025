820421e7408c460d13f2e17b25f6c01af4b75226
Pablo <contact@pablo.town>
2025-07-27 18:06:06 +0200

    fix history navigation

--- projetpixel2/scripts/commands/console.gd
+++ projetpixel2/scripts/commands/console.gd
@@ -6,13 +6,17 @@ class_name Console
 
 var pre_parsing: Dictionary[String, Variant]
 var commands_history: Array[String] = []
-var history_index = 0
+var history_index = len(commands_history)
 var is_tab_selecting = false
 
 func restore_focus():
 	await get_tree().process_frame
 	prompt.release_focus()
 	prompt.grab_focus()
+
+func add_to_history(command: String) -> void:
+	commands_history.append(command)
+	history_index = len(commands_history) - 1
 
 func _input(event: InputEvent) -> void:
 	if prompt.has_focus():
@@ -27,20 +31,20 @@ func _input(event: InputEvent) -> void:
 			var command = Parser.parse(command_text, logger)
 			if command.error:
 				if command_text != "":
-					commands_history.append(command_text)
+					add_to_history(command_text)
 				logger.print(command.error_message, logger.log_types.ERROR)
 				restore_focus()
 				return
 			command.execute()
-			commands_history.append(command_text)
+			add_to_history(command_text)
 			restore_focus()
 		if Input.is_key_pressed(KEY_UP):
 			if len(commands_history) > 0:
 				prompt.text = commands_history[history_index]
-				if history_index >= len(commands_history) - 1:
-					history_index = 0
+				if history_index == 0:
+					history_index = len(commands_history) - 1
 				else:
-					history_index += 1
+					history_index -= 1
 		if Input.is_key_pressed(KEY_TAB):
 			# TODO: tab completion
 			pass
