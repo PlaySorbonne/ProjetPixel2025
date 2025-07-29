4a0665e29635824e477208289a295e5f9c8804ee
Maxou <78730104+Maxoups@users.noreply.github.com>
2025-07-29 11:04:31 +0200

    wip xp object

---
+++ projetpixel2/scripts/experience_drop.gd
@@ -0,0 +1,30 @@
+extends Area3D
+class_name ExperienceDrop
+
+
+class ExperienceLevel:
+	var xp_amount : int
+	var xp_color : Color
+	
+	func _init(xp_amount: int = 0, xp_color: Color = Color.WHITE) -> void:
+		self.xp_amount = xp_amount
+		self.xp_color = xp_color
+
+static var experience_thresholds : Array[ExperienceLevel] = [
+	ExperienceLevel.new(1600, Color.WHITE),
+	ExperienceLevel.new(800, Color.AQUA),
+	ExperienceLevel.new(400, Color.BLUE),
+	ExperienceLevel.new(200, Color.PURPLE),
+	ExperienceLevel.new(0, Color.RED),
+]
+
+
+@export var experience_points := 100
+
+
+func _apply_color() -> void:
+	for xp_level : ExperienceLevel in experience_thresholds:
+		if experience_points > xp_level.xp_amount :
+			var xp_mat : StandardMaterial3D = $CSGSphere3D.material
+			return
+	print_debug("wtf, negative xp => " + str(experience_points) + "?")
