a689af8c6ab67c481ae40234be8d8a08682c748e
Maxou <mm.maxencemaire@gmail.com>
2025-08-17 17:24:29 +0200

    added time limit on explosion object

--- projetpixel2/scripts/explosion_base.gd
+++ projetpixel2/scripts/explosion_base.gd
@@ -2,6 +2,10 @@ extends Area3D
 class_name ExplosionBase
 
 
+@export var duration := 1.0:
+	set(value):
+		duration = value
+		$TimerLifetime.start(duration)
 @export var radius := 2.5
 
 
@@ -11,3 +15,6 @@ func apply_effect(enemy : BaseEnemy) -> void:
 func _on_body_entered(body: Node3D) -> void:
 	if body is BaseEnemy:
 		apply_effect(body)
+
+func _on_timer_lifetime_timeout() -> void:
+	queue_free()
