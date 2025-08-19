a8e043f1b4a3d34633a4bd48f3a607d90e80a34c
Maxoups <mm.maxencemaire@gmail.com>
2025-08-19 15:36:40 +0200

    finished totem_shoot

--- projetpixel2/scripts/totem_base.gd
+++ projetpixel2/scripts/totem_base.gd
@@ -6,10 +6,13 @@ var tower : TowerBase
 
 
 func trigger_effect() -> void:
-	pass
+	$TimeAfterTrigger.start()
 
 func stack_totem() -> void:
 	pass
 
 func destroy_totem() -> void:
 	queue_free()
+
+func _on_time_after_trigger_timeout() -> void:
+	destroy_totem()
--- projetpixel2/scripts/totem_shoot.gd
+++ projetpixel2/scripts/totem_shoot.gd
@@ -1,14 +1,27 @@
 extends TotemBase
 class_name TotemShoot
 
+
 var number_of_projectiles := 1
-var projectile_template : Projectile
+var current_projectile := 0
+var is_instant := false
 
 
 func trigger_effect() -> void:
-	for _i : int in range(number_of_projectiles):
-		shoot()
-	destroy_totem()
+	if is_instant:
+		for _i : int in range(number_of_projectiles):
+			shoot()
+		super.trigger_effect()
+	else:
+		if current_projectile < number_of_projectiles:
+			current_projectile += 1
+			shoot()
+			$TimerShoot.start()
+		else:
+			super.trigger_effect()
+
+func _on_timer_shoot_timeout() -> void:
+	trigger_effect()
 
 func stack_totem() -> void:
 	number_of_projectiles += 1
