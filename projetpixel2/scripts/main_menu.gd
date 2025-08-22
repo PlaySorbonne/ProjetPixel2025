9e58f20114ab3ff4b5183d3c8da2902140f63d21
Maxou <mm.maxencemaire@gmail.com>
2025-08-22 14:05:43 +0200

    added cards debug screen

--- projetpixel2/scripts/main_menu.gd
+++ projetpixel2/scripts/main_menu.gd
@@ -6,9 +6,6 @@ class_name MainMenu
 func _on_button_tutorial_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/world/world_tuto.tscn")
 
-func _on_button_debug_cards_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/interface/menus/submenus/cards_test_menu.tscn")
-
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
 	
