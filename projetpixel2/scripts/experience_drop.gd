4c971871ed30fc3283f0d8e09f9f1f4234200c40
Maxoups <mm.maxencemaire@gmail.com>
2025-09-06 13:41:31 +0200

    remove hp and size scaling when merging xp drops

--- projetpixel2/scripts/experience_drop.gd
+++ projetpixel2/scripts/experience_drop.gd
@@ -10,6 +10,8 @@ class ExperienceLevel:
 		self.xp_amount = xp_amount
 		self.xp_color = xp_color
 
+
+const MAX_HP := 100
 const NB_ORBS_MERGE := 3
 
 static var experience_thresholds : Array[ExperienceLevel] = [
@@ -21,13 +23,13 @@ static var experience_thresholds : Array[ExperienceLevel] = [
 	ExperienceLevel.new(0, Color.BLACK),
 ]
 
-static func spawn_xp(pos : Vector3, xp : int, fast_spawn := false, hp := 100) -> void:
+static func spawn_xp(pos : Vector3, xp : int, fast_spawn := false) -> void:
 	const EXPERIENCE_DROP_RES := preload("res://scenes/world/enemies/experience_drop.tscn")
 	var experience_drop := EXPERIENCE_DROP_RES.instantiate()
 	GV.world.add_child(experience_drop)
 	experience_drop.position = pos
 	experience_drop.experience_points = xp
-	experience_drop.hitpoints = hp
+	experience_drop.hitpoints = MAX_HP
 	if not fast_spawn:
 		experience_drop.create_experience_object()
 
@@ -35,10 +37,7 @@ static func spawn_xp(pos : Vector3, xp : int, fast_spawn := false, hp := 100) ->
 signal xp_drop_collected(xp_amount : int)
 
 
-@export var hitpoints := 100:
-	set(value):
-		hitpoints = value
-		$MeshInstance3D.scale = Vector3.ONE * log(hitpoints / 100)
+@export var hitpoints := MAX_HP
 @export var experience_points := 100:
 	set(value):
 		experience_points = value
@@ -46,7 +45,6 @@ signal xp_drop_collected(xp_amount : int)
 var xp_level_index : int = 0:
 	set(value):
 		xp_level_index = value
-		#print("spawn orb -> xp=" + str(experience_points) + " ; hp=" + str(hitpoints) + " ; level = " + str(xp_level_index))
 		_check_neighbor_xp_drops()
 var marked_for_deletion := false
 var is_being_harvested := false
@@ -91,16 +89,13 @@ func destroy_experience_object() -> void:
 func merge_xp_drops(neighbors_xp : Array[ExperienceDrop]) -> void:
 	# remove the neighbor and move self to mid point
 	var new_xp := 0
-	var new_hp := 0
 	var new_pos := Vector3.ZERO
 	for neighbor : ExperienceDrop in neighbors_xp:
 		new_pos += neighbor.position
 		new_xp += neighbor.experience_points
-		new_hp += neighbor.hitpoints
 		neighbor.marked_for_deletion = true
 	new_pos /= len(neighbors_xp)
 	new_xp *= 1.1
-	new_hp *= 1.3
 	var t := get_tree().create_tween().set_trans(Tween.TRANS_CUBIC).set_parallel()
 	for neighbor : ExperienceDrop in neighbors_xp:
 		t.tween_property(neighbor, "position", new_pos, 0.4)
@@ -108,8 +103,7 @@ func merge_xp_drops(neighbors_xp : Array[ExperienceDrop]) -> void:
 	spawn_xp(
 		new_pos,
 		new_xp,
-		true,
-		new_hp
+		true
 	)
 	for neighbor : ExperienceDrop in neighbors_xp:
 		neighbor.queue_free()
