0fd57a856682f8eb71cb1bae473a09c81d2a1f8e
Maxoups <mm.maxencemaire@gmail.com>
2025-09-06 11:24:24 +0200

    added festival demo button

--- projetpixel2/scripts/main_menu.gd
+++ projetpixel2/scripts/main_menu.gd
@@ -7,10 +7,11 @@ func _on_button_tutorial_pressed() -> void:
 
 func _on_button_quit_pressed() -> void:
 	get_tree().quit()
-	
-
-
 
 func _on_button_debug_world_pressed() -> void:
 	GV.debug_mode = true
+	get_tree().change_scene_to_file(GV.WORLD_FILE)
+
+func _on_button_festival_demo_pressed() -> void:
+	print_debug("ADD FESTIVAL DEMO TUTO")
 	get_tree().change_scene_to_file(GV.WORLD_FILE)
