f3f65f704f3f82b52694fd2d8d30c6c98980e9fd
Maxoups <mm.maxencemaire@gmail.com>
2025-10-29 22:34:48 +0100

    added cards scroll & visibility spacer in craft menu

--- projetpixel2/scripts/craft_menu.gd
+++ projetpixel2/scripts/craft_menu.gd
@@ -2,11 +2,15 @@ extends Submenu
 class_name CraftMenu
 
 
-@onready var card_container := $DeckCraft/CardContainer
+@onready var spacer := $DeckCraft/CardsScrollContainer/CardContainer/Spacer
+@onready var card_container := $DeckCraft/CardsScrollContainer/CardContainer
+
 
 func _ready() -> void:
 	#print("CardData.cards_data = ")
 	#print(CardData.cards_data)
+	card_container.custom_minimum_size = $DeckCraft/CardsScrollContainer.size
+	spacer.custom_minimum_size.x = card_container.custom_minimum_size.x
 	for card : CardData in CardData.cards_data.values():
 		var card_obj := CardObject.create_card_object(card)
 		card_container.add_child(card_obj)
