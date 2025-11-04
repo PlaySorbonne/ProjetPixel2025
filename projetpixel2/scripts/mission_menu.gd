e698a7e5c0ec87e27d85daaa3755ac270efb3bcd
Maxoups <mm.maxencemaire@gmail.com>
2025-11-04 09:54:15 +0100

    saved deck craft as scene

--- projetpixel2/scripts/mission_menu.gd
+++ projetpixel2/scripts/mission_menu.gd
@@ -2,8 +2,8 @@ extends Submenu
 class_name MissionMenu
 
 
-
-
+func _ready() -> void:
+	$ButtonDeck/LabelDeck.text = SaveData.player_decks[SaveData.selected_deck].name
 
 func _on_button_play_pressed() -> void:
 	GV.persistent_menu_world.camera_movement(
@@ -14,3 +14,6 @@ func _on_button_play_pressed() -> void:
 	tween.tween_property($FadeTransition, "color", Color.BLACK, 0.5)
 	await tween.finished
 	get_tree().change_scene_to_file(GV.WORLD_FILE)
+
+func _on_button_deck_pressed() -> void:
+	pass # Replace with function body.
