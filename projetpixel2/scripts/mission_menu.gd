b21cccecb948cf88d91f271b7eaed45e3bbae40e
Maxoups <mm.maxencemaire@gmail.com>
2025-11-05 10:31:02 +0100

    added deck craft to mission menu

--- projetpixel2/scripts/mission_menu.gd
+++ projetpixel2/scripts/mission_menu.gd
@@ -3,7 +3,12 @@ class_name MissionMenu
 
 
 func _ready() -> void:
-	$ButtonDeck/LabelDeck.text = SaveData.player_decks[SaveData.selected_deck].name
+	update_deck_button()
+
+func update_deck_button() -> void:
+	var current_deck := SaveData.player_decks[SaveData.selected_deck]
+	$ButtonDeck/LabelDeck.text = current_deck.name
+	$ButtonDeck.icon = Deck.DECK_TEXTURES[current_deck.texture_index]
 
 func _on_button_play_pressed() -> void:
 	GV.persistent_menu_world.camera_movement(
@@ -16,4 +21,9 @@ func _on_button_play_pressed() -> void:
 	get_tree().change_scene_to_file(GV.WORLD_FILE)
 
 func _on_button_deck_pressed() -> void:
-	pass # Replace with function body.
+	$DeckCraft.visible = true
+	$DeckCraft.initialize_cards()
+
+func _on_button_deck_craft_back_pressed() -> void:
+	$DeckCraft.visible = false
+	update_deck_button()
