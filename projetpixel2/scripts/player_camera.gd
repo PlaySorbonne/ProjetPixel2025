a2fdb4c6ab6ccda784e99eb5824ab1f417314593
Maxoups <mm.maxencemaire@gmail.com>
2025-11-10 14:41:20 +0100

    added 2d camera movements during game

--- projetpixel2/scripts/player_camera.gd
+++ projetpixel2/scripts/player_camera.gd
@@ -2,7 +2,9 @@ extends Camera3D
 class_name PlayerCamera
 
 
-const SPEED := 50.0
+const SPEED := 15.0
+
+var current_speed := 0.0
 
 
 func _ready() -> void:
@@ -14,4 +16,12 @@ func _physics_process(delta: float) -> void:
 		direction += Vector3.FORWARD
 	if Input.is_action_pressed("move_backward"):
 		direction += Vector3.BACK
-	position += (direction * SPEED * delta)
+	if Input.is_action_pressed("move_left"):
+		direction += Vector3.LEFT
+	if Input.is_action_pressed("move_right"):
+		direction += Vector3.RIGHT
+	if direction != Vector3.ZERO:
+		current_speed = lerp(current_speed, SPEED, 0.01 * delta)
+		position += (direction * SPEED * delta)
+	else:
+		current_speed = 0.0
