813f3d071f04a5a08aa0aa076a418abdf9cb7fe0
Maxoups <mm.maxencemaire@gmail.com>
2025-11-17 15:40:00 +0100

    changed exp popup animation

--- projetpixel2/scripts/damage_popup.gd
+++ projetpixel2/scripts/damage_popup.gd
@@ -13,7 +13,6 @@ const POSSIBLE_OFFSETS : Array[Vector3] = [
 	Vector3(1, 0, 0),
 	Vector3(1, 0, 1),
 ]
-const ANIM_TIME := 1.0
 
 
 static func display_experience(amount : int, spawn_pos : Vector3) -> void:
@@ -21,13 +20,14 @@ static func display_experience(amount : int, spawn_pos : Vector3) -> void:
 	popup.text = str(amount)
 	popup.modulate = Color.BLUE
 	popup.font_size = 74
-	popup.global_position = spawn_pos + Vector3(0.0, 0.0, 0.0)
+	popup.global_position = spawn_pos + Vector3(0.0, 0.5, 0.0)
 	#const EXP_POPUP_SPAWN_RADIUS := 1.0
 	#popup.global_position = GV.space_ship.global_position + Vector3(
 		#randf_range(-EXP_POPUP_SPAWN_RADIUS, EXP_POPUP_SPAWN_RADIUS),
 		 #1.75, randf_range(-EXP_POPUP_SPAWN_RADIUS, EXP_POPUP_SPAWN_RADIUS))
 	popup.pick_random_direction = false
-	popup.direction = Vector3.UP
+	popup.direction = Vector3.ZERO
+	popup.popup_animation = popup.play_anim_size
 	GV.world.add_child(popup)
 
 static func display_damage(entity : Node3D, damage : int, is_crit : bool) -> void:
@@ -36,24 +36,36 @@ static func display_damage(entity : Node3D, damage : int, is_crit : bool) -> voi
 	if is_crit:
 		popup.modulate = Color.DARK_RED
 		popup.font_size = 74
-	popup.position = entity.position + Vector3(0, 0.25, 0)
+	popup.position = entity.position + Vector3(0, 0.2, 0)
 	GV.world.add_child(popup)
 
 
 var direction := Vector3.ZERO
 var pick_random_direction := true
+var popup_animation : Callable = play_anim_color
 
 
 func _process(delta: float) -> void:
 	position += direction * delta
 
 func _ready() -> void:
-	$Timer.start(ANIM_TIME)
 	if pick_random_direction:
 		direction = POSSIBLE_OFFSETS.pick_random()
+	var t : Tween = popup_animation.call()
+	t.finished.connect(_on_anim_finished)
+
+func play_anim_size() -> Tween:
+	var t := create_tween()
+	t.tween_property(self, "scale", scale * 2.7, 0.25).set_ease(Tween.EASE_IN)
+	t.tween_property(self, "scale", Vector3.ZERO, 0.3).set_ease(Tween.EASE_OUT).set_delay(0.15)
+	return t
+
+func play_anim_color() -> Tween:
+	const ANIM_TIME := 1.0
 	var final_color := Color(modulate.r, modulate.g, modulate.b, 0.0)
 	var t := create_tween().set_parallel().set_ease(Tween.EASE_OUT)
 	t.tween_property(self, "modulate", final_color, ANIM_TIME)
+	return t
 
-func _on_timer_timeout() -> void:
+func _on_anim_finished() -> void:
 	queue_free()
