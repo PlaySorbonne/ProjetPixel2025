7f52f86ae566fee52b7dd5a523469e505a0b2aae
Maxoups <mm.maxencemaire@gmail.com>
2025-11-17 16:00:21 +0100

    fixed persistent menu -> submenu transitions

--- projetpixel2/scripts/persistent_menu.gd
+++ projetpixel2/scripts/persistent_menu.gd
@@ -2,8 +2,6 @@ extends Node
 class_name PersistentMenu
 
 
-const MENU_TRANSITION_TIME := 0.3
-
 @onready var current_menu : Submenu = $CanvasLayer/MainMenu
 @onready var current_pos := Vector2.ZERO
 
@@ -19,14 +17,15 @@ func transition_to_scene() -> void:
 	pass
 
 func transition_to_menu(new_menu : Submenu) -> void:
-	var current_menu_transition := current_pos - new_menu.submenu_pos
+	const MENU_TRANSITION_TIME := 0.3
+	var old_menu := current_menu
 	var tween := get_tree().create_tween().set_trans(Tween.TRANS_CUBIC).set_parallel()
 	$CanvasLayer.add_child(new_menu)
+	current_menu = new_menu
 	new_menu.position = new_menu.size * new_menu.submenu_pos - current_pos
 	tween.tween_property(new_menu, "position", Vector2.ZERO, MENU_TRANSITION_TIME)
-	tween.tween_property(current_menu, "position", 
-				current_menu.size * current_menu_transition, MENU_TRANSITION_TIME)
-	
-	current_menu.queue_free()
-	current_menu = new_menu
+	tween.tween_property(old_menu, "position", 
+				old_menu.size * new_menu.submenu_pos * -1, MENU_TRANSITION_TIME)
+	await tween.finished
+	old_menu.queue_free()
 	current_pos = new_menu.submenu_pos
