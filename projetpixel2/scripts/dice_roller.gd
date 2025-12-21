14e0aa1ba59c14a2cd9fa474f999086239d3717b
Maxoups <mm.maxencemaire@gmail.com>
2025-12-21 17:03:05 +0100

    Update dice_roller.gd

--- projetpixel2/scripts/dice_roller.gd
+++ projetpixel2/scripts/dice_roller.gd
@@ -4,4 +4,5 @@ class_name DiceRoller
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		Dice.roll_dice(self, $Marker3D.position)
+		for _i : int in range(5):
+			Dice.roll_dice(self, $Marker3D.position)
