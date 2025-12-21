d6538f399bd36c2cc7e64da2512a3f4bced09731
Maxoups <mm.maxencemaire@gmail.com>
2025-12-21 17:02:12 +0100

    fixed dice faces

--- projetpixel2/scripts/dice_six_sides.gd
+++ projetpixel2/scripts/dice_six_sides.gd
@@ -19,13 +19,20 @@ static func roll_dice(parent : Node3D, dice_location : Vector3) -> Dice:
 @export var roll_torque := 10.0
 
 
+func _ready() -> void:
+	set_physics_process(false)
+
+func _on_timer_timeout() -> void:
+	set_physics_process(true)
+
 func _physics_process(_delta : float):
-	return
 	if is_still() and sleeping == false:
 		sleeping = true
 		var value := get_dice_value()
 		print("Dice rolled:", value)
 		dice_rolled.emit(value)
+		freeze = true
+		scale = Vector3(0.75, 0.75, 0.75)
 
 func roll() -> void:
 	rotation = Vector3(
@@ -58,23 +65,23 @@ func is_still() -> bool:
 	return linear_velocity.length() < 0.05 and angular_velocity.length() < 0.05
 
 func get_dice_value() -> int:
-	var up := global_transform.basis * Vector3.UP
-	
-	var faces := {
-		1: Vector3.UP,
-		6: Vector3.DOWN,
-		2: Vector3.FORWARD,
-		5: Vector3.BACK,
-		3: Vector3.RIGHT,
-		4: Vector3.LEFT
+	const FACES := {
+		1: Vector3.FORWARD,
+		6: Vector3.BACK,
+		2: Vector3.RIGHT,
+		5: Vector3.LEFT,
+		3: Vector3.UP,
+		4: Vector3.DOWN
 	}
-
+	
 	var best_dot := -1.0
 	var best_face := 1
-	
-	for face in faces:
-		var dot := up.dot(global_transform.basis * faces[face])
+	print("get dice value")
+	for face in FACES:
+		var dot := Vector3.UP.dot(global_transform.basis * FACES[face])
+		print("   dot["+ str(face) +"] = " + str(dot))
 		if dot > best_dot:
+			print("       new best dot!")
 			best_dot = dot
 			best_face = face
 	
