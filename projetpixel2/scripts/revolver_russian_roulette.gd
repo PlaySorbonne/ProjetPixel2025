180dc8367b5263b120a8309ee83ba24b0a34d03f
Maxoups <mm.maxencemaire@gmail.com>
2025-12-22 16:48:15 +0100

    added russian roulette to hud

--- projetpixel2/scripts/revolver_russian_roulette.gd
+++ projetpixel2/scripts/revolver_russian_roulette.gd
@@ -1,2 +1,16 @@
 extends MeshInstance3D
 class_name RevolverRussianRoulette
+
+
+signal revolver_shot(result : bool)
+
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):
+		pass
+
+func shoot_revolver() -> void:
+	pass
+
+func _ready() -> void:
+	pass
