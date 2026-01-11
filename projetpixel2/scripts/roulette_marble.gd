15ea1bab9f91af7985164f94e88bd1f342e0a5f5
Maxoups <mm.maxencemaire@gmail.com>
2026-01-11 15:53:20 +0100

    added rollng marbles to casino wheel

--- projetpixel2/scripts/roulette_marble.gd
+++ projetpixel2/scripts/roulette_marble.gd
@@ -2,57 +2,62 @@ extends MeshInstance3D
 class_name RouletteMarble
 
 
+signal marble_landed(slot : int)
+
+const MARBLE_RES := preload("res://scenes/casino/roulette_marble.tscn")
 const TOTAL_SLOTS := 37
 const SLOTS := [
 	0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 
 	5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26
 ]
+const BALL_ROLLING_TIME := 4.25
 
 @export var radius := 2.375
-@export var angular_speed := 4.0   # radians/sec
 @export var height := 0.2
 
 var angle := 0.0
 var center := Vector3.ZERO
 var running := false
+var slot := 0
+var previous_tween : Tween
 
+
+static func launch_marble(roulette : RouletteWheel, slot : int) -> RouletteMarble:
+	var marble := MARBLE_RES.instantiate()
+	roulette.RouletteSpinny.add_child(marble)
+	marble.launch_to_slot(slot)
+	return marble
 
 func launch_to_slot(slot_index: int)-> void:
-	var target_angle := SLOTS.find(slot_index) * TAU / TOTAL_SLOTS
-	set_position_from_angle(target_angle)
+	if is_instance_valid(previous_tween):
+		previous_tween.kill()
 	
-	return
-	var extra_turns = randi_range(3, 6)
-	var spiral_time := randf_range(3.5, 4.5)
-	
-	running = true
-	
+	slot = slot_index
 	var start_angle := randf() * TAU
-	set_position_from_angle(start_angle)
-	
-	var total_rotation = (target_angle - start_angle) + TAU * extra_turns
-	var speed : float = total_rotation / spiral_time
-	start_motion(start_angle, speed)
-
-func set_position_from_angle(angle : float) -> void:
-	angle -= PI/2.0
-	position = Vector3(
-		center.x + cos(angle) * radius, 
-		center.z + height, 
-		center.y + sin(angle) * radius
-	)
-
-func start_motion(start_angle := 0.0, speed := 1.0):
-	angular_speed = speed
 	angle = start_angle
+	var target_angle := SLOTS.find(slot_index) * TAU / TOTAL_SLOTS
+	var roll_time := randf_range(0.0, 1.5) + BALL_ROLLING_TIME
+	
+	var t := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	t.tween_property(self, "angle", target_angle, roll_time)
+	t.finished.connect(stop_motion)
+	
+	previous_tween= t
 	running = true
+
+func set_position_from_angle(marble_angle : float) -> void:
+	marble_angle -= PI/2.0
+	position = Vector3(
+		center.x + cos(marble_angle) * radius, 
+		center.z + height, 
+		center.y + sin(marble_angle) * radius
+	)
 
 func stop_motion() -> void:
 	running = false
+	marble_landed.emit(slot)
 
-func _process(delta : float)-> void:
+func _process(_delta : float)-> void:
 	if not running:
 		return
-	
-	angle += angular_speed * delta
 	set_position_from_angle(angle)
--- projetpixel2/scripts/roulette_wheel.gd
+++ projetpixel2/scripts/roulette_wheel.gd
@@ -2,20 +2,31 @@ extends Node3D
 class_name RouletteWheel
 
 
+const ROULETTE_SPIN_TIME := 2.1
+const ROULETTE_SPIN_ANGLE := 0.8
+
 @export var number_of_marbles := 1
+
+@onready var RouletteSpinny := $RouletteSpinny
 
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		spin_roulette()
 
-
 func spin_roulette() -> void:
-	
+	var final_wheel_rotation : Vector3= RouletteSpinny.rotation + Vector3(
+		0.0, 
+		(randf_range(0.0, 0.1) + ROULETTE_SPIN_ANGLE)* TAU, 
+		0.0
+	)
+	var spin_time := randf_range(0.0, 0.25) + ROULETTE_SPIN_TIME
+	var t := create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
+	t.tween_property(RouletteSpinny, "rotation", final_wheel_rotation, spin_time)
 	for _i in range(number_of_marbles):
 		var slot := randi_range(0, 36)
 		launch_marble(slot)
 
 func launch_marble(slot : int) -> void:
 	$CanvasLayer/Label.text = "Slot = " + str(slot)
-	$RouletteSpinny/RouletteMarble.launch_to_slot(slot)
+	RouletteMarble.launch_marble(self, slot)
