eb77b6153c9a99be2804ff854bfe0d22097129d4
Maxoups <mm.maxencemaire@gmail.com>
2026-01-12 10:21:00 +0100

    wip player hud

--- projetpixel2/scripts/casino_window.gd
+++ projetpixel2/scripts/casino_window.gd
@@ -1,2 +1,10 @@
 extends Control
 class_name CasinoWindow
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			$DragAndDrop2D.press()
+		else:
+			$DragAndDrop2D.release()
