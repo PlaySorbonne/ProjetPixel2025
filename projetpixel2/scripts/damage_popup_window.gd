ed1a32d8e92f895b14070fc43b9c64cac0ad4d32
Maxoups <mm.maxencemaire@gmail.com>
2026-01-14 14:08:21 +0100

    wip warning popup

--- projetpixel2/scripts/damage_popup_window.gd
+++ projetpixel2/scripts/damage_popup_window.gd
@@ -6,12 +6,16 @@ const WARNING_POPUP_RES := preload("res://scenes/interface/casino_minigames/warn
 
 static func spawn_warning_popup(warning_msg : String) -> void:
 	var popup : WarningPopupWindow = WARNING_POPUP_RES.instantiate()
-	popup
+	popup.message = warning_msg
 	spawn_popup(popup)
+
+
+var message : String = "Hull breach"
 
 
 func _ready() -> void:
 	super._ready()
+	$Contents/ColorRect/LabelMessage.text = message
 	random_popup_position()
 
 func _on_window_opened() -> void:
