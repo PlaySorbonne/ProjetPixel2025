fd59d839378f327a8cbc4589b3b5b022ee250615
Maxoups <mm.maxencemaire@gmail.com>
2026-03-09 09:07:58 +0100

    added auto collect xp

--- projetpixel2/scripts/experience_drop.gd
+++ projetpixel2/scripts/experience_drop.gd
@@ -55,15 +55,22 @@ var is_mouse_pressed := false
 var scale_tween : Tween
 
 
-func _process(delta: float) -> void:
-	if is_mouse_over and Input.is_action_pressed("click"
-			) and not marked_for_deletion and not GV.is_dragging_object:
-		marked_for_deletion = true
-		xp_drop_collected.emit(experience_points)
-		is_being_harvested = false
-		set_process(false)
-		await get_tree().create_timer(0.2).timeout
-		destroy_experience_object()
+#func _process(delta: float) -> void:
+	#if is_mouse_over and Input.is_action_pressed("click"
+			#) and not marked_for_deletion and not GV.is_dragging_object:
+		#marked_for_deletion = true
+		#xp_drop_collected.emit(experience_points)
+		#is_being_harvested = false
+		#set_process(false)
+		#await get_tree().create_timer(0.2).timeout
+		#destroy_experience_object()
+
+func _ready() -> void:
+	marked_for_deletion = true
+	xp_drop_collected.emit(experience_points)
+	is_being_harvested = false
+	await get_tree().create_timer(0.2).timeout
+	destroy_experience_object()
 
 func start_mining() -> void:
 	is_being_harvested = true
@@ -96,11 +103,11 @@ func create_experience_object() -> void:
 	t.tween_property(self, "scale", Vector3.ONE, 0.15)
 
 func destroy_experience_object() -> void:
-	RunData.gain_experience(experience_points)
 	DamagePopup.display_experience(experience_points, global_position)
 	var t := get_tree().create_tween().set_trans(Tween.TRANS_CUBIC)
 	t.tween_property(self, "global_position", GV.space_ship.global_position, 0.4)
 	await t.finished
+	RunData.gain_experience(experience_points)
 	queue_free()
 
 func merge_xp_drops(neighbors_xp : Array[ExperienceDrop]) -> void:
