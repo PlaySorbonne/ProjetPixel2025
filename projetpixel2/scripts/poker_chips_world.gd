c4610d88f264678968821558d58d04d3d5f907f6
Maxoups <mm.maxencemaire@gmail.com>
2026-03-12 09:49:54 +0100

    added remove chips function

--- projetpixel2/scripts/poker_chips_world.gd
+++ projetpixel2/scripts/poker_chips_world.gd
@@ -27,15 +27,25 @@ const POKER_CHIP_WIDTH := 0.032
 var poker_chip_tweens : Dictionary[PokerChip.ChipValues, Tween]
 
 
-func add_chip(chip_value : PokerChip.ChipValues) -> void:
+func _ready() -> void:
+	for val : PokerChip.ChipValues in PokerChip.ChipValues.values():
+		poker_chip_tweens[val] = null
+	for obj : Node in get_children():
+		if obj.is_in_group("chips_to_delete"):
+			obj.queue_free()
+
+func _update_chip_tween(chip_value : PokerChip.ChipValues) -> Tween:
 	if poker_chip_tweens[chip_value]:
 		poker_chip_tweens[chip_value].kill()
+	return create_tween().set_ease(Tween.EASE_OUT).set_parallel()
+
+func add_chip(chip_value : PokerChip.ChipValues) -> void:
 	var new_chip := PokerChip.spawn_poker_chip(
 		self, 
 		poker_chip_positions[chip_value] - Vector3(0.0, 0.3, 0.0),
 		chip_value
 	)
-	var t := create_tween().set_ease(Tween.EASE_OUT).set_parallel()
+	var t := _update_chip_tween(chip_value)
 	for chip_index : int in poker_chip_objects[chip_value].size():
 		var current_pos : Vector3 = poker_chip_objects[chip_value][chip_index].position
 		t.tween_property(
@@ -57,12 +67,39 @@ func add_chip(chip_value : PokerChip.ChipValues) -> void:
 	poker_chip_tweens[chip_value] = t
 	poker_chip_objects[chip_value].insert(0, new_chip)
 
-func _ready() -> void:
-	for val : PokerChip.ChipValues in PokerChip.ChipValues.values():
-		poker_chip_tweens[val] = null
-	for obj : Node in get_children():
-		if obj.is_in_group("chips_to_delete"):
-			obj.queue_free()
+func remove_chip(chip_value : PokerChip.ChipValues) -> bool:
+	var nb_chips := get_number_of_chips(chip_value)
+	if nb_chips == 0:
+		return false
+	var removed_chip : PokerChip = poker_chip_objects[chip_value][nb_chips-1]
+	var t : Tween = create_tween().set_ease(Tween.EASE_OUT).set_parallel()
+	t.tween_property(
+		removed_chip, 
+		"position", 
+		removed_chip.position+Vector3(0.0, 0.5, 0.0), 
+		0.3
+	)
+	t.tween_property(
+		removed_chip,
+		"global_rotation",
+		removed_chip.global_rotation + Vector3(0.0, 0.0, 9.5),
+		0.3
+	)
+	t.tween_property(
+		removed_chip,
+		"scale",
+		Vector3.ZERO,
+		0.1
+	).set_delay(0.2)
+	t.finished.connect(removed_chip.queue_free)
+	poker_chip_objects[chip_value].resize(nb_chips-1)
+	return true
+
+func get_number_of_chips(value : PokerChip.ChipValues) -> int:
+	return len(poker_chip_objects[value])
+
+func remove_random_chip() -> bool:
+	return remove_chip(PokerChip.ChipValues.values().pick_random())
 
 func add_random_chip() -> void:
 	add_chip(PokerChip.ChipValues.values().pick_random())
@@ -70,3 +107,5 @@ func add_random_chip() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("debug_m"):
 		add_random_chip()
+	if Input.is_action_just_pressed("debug_l"):
+		remove_random_chip()
