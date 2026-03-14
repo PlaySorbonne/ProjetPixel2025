253cd3f1eb46f058c49b95c1211f0c84c683d267
Maxoups <mm.maxencemaire@gmail.com>
2026-03-14 15:36:24 +0100

    visual ui updates

--- projetpixel2/scripts/common_booster.gd
+++ projetpixel2/scripts/common_booster.gd
@@ -10,5 +10,4 @@ static func spawn_booster(nparent : Node) -> Booster:
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	super._on_animation_player_animation_finished(_anim_name)
 	draw_cards(3, CardData.CardRarities.Common)
-	await get_tree().create_timer(1.2).timeout
 	destroy_booster()
