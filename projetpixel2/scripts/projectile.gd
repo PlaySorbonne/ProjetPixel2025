ecadb6ea421141b48860c0fe356730c48ec4d3f2
Maxoups <mm.maxencemaire@gmail.com>
2026-03-14 17:14:40 +0100

    finished revamped tower info window

--- projetpixel2/scripts/projectile.gd
+++ projetpixel2/scripts/projectile.gd
@@ -2,15 +2,42 @@ extends Resource
 class_name Projectile
 
 
+signal projectile_updated
+
+
 var damage_expression : Callable = default_damage_expression # expression used to compute the damage done by the projectile.
-@export var speed : float = 1.0 # speed multiplier of the projectile
-@export var damage : int = 50 # base damage of the projectile
-@export var size := 1.0 # size multiplier of the projectile
-@export var pierce := 0 # number of times the projectile can pierce an enemy
-@export var bounce := 0 # number of times the projectile can bounce on an enemy
-@export var damage_type := DamageableObject.DamagingTypes.Neutral # type of the projectile
-@export var critical_hit_chance := 0.01 # chance of a critical hit
-@export var critical_hit_intensity := 10.0 # critical hit damage multiplier
+@export var speed : float = 1.0: # speed multiplier of the projectile
+	set(value):
+		speed = value
+		projectile_updated.emit()
+@export var damage : int = 50: # base damage of the projectile
+	set(value):
+		damage = value
+		projectile_updated.emit()
+@export var size := 1.0: # size multiplier of the projectile
+	set(value):
+		size = value
+		projectile_updated.emit()
+@export var pierce := 0: # number of times the projectile can pierce an enemy
+	set(value):
+		pierce = value
+		projectile_updated.emit()
+@export var bounce := 0: # number of times the projectile can bounce on an enemy
+	set(value):
+		bounce = value
+		projectile_updated.emit()
+@export var damage_type := DamageableObject.DamagingTypes.Neutral: # type of the projectile
+	set(value):
+		damage_type = value
+		projectile_updated.emit()
+@export var critical_hit_chance := 0.01: # chance of a critical hit
+	set(value):
+		critical_hit_chance = value
+		projectile_updated.emit()
+@export var critical_hit_intensity := 10.0: # critical hit damage multiplier
+	set(value):
+		critical_hit_intensity = value
+		projectile_updated.emit()
 
 
 func default_damage_expression() -> int:
--- projetpixel2/scripts/tower_info_window.gd
+++ projetpixel2/scripts/tower_info_window.gd
@@ -7,13 +7,30 @@ const CARD_INFOS := preload("res://scenes/interface/gameplay_hud/info_popups/car
 
 var tower : TowerBase
 @onready var cards_container := $Contents/ScrollContainer/VBoxContainer
+@onready var label_stat_vals_1 := $Contents/LabelStatsVals1
+@onready var label_stat_vals_2 := $Contents/LabelStatsVals2
 
 
 func _ready() -> void:
 	super._ready()
 	tower.tower_card_added.connect(_add_card_infos)
+	tower.projectile_updated.connect(_update_tower_stats)
 	for card : CardData in tower.cards:
 		_add_card_infos(card)
+	_update_tower_stats()
+
+func _update_tower_stats() -> void:
+	label_stat_vals_1.text = \
+	str(tower.projectile_template.damage)  + "\n"\
+	+ str(tower.projectile_template.speed) + "\n"\
+	+ str(tower.projectile_template.size)  + "\n"\
+	+ str(tower.projectile_template.critical_hit_chance)
+	label_stat_vals_2.text = \
+	str(tower.projectile_template.critical_hit_intensity) + "\n"\
+	+ str(tower.projectile_template.pierce) + "\n"\
+	+ str(tower.projectile_template.bounce) + "\n"\
+	+ str(tower.projectile_template.damage_type) 
+	
 
 func _add_card_infos(new_card : CardData) -> void:
 	var card_infos := CARD_INFOS.instantiate()
