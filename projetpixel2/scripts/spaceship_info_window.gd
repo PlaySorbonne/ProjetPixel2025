891ee9815092779099ba82d9ab98c4ce8f9e0d98
Maxoups <mm.maxencemaire@gmail.com>
2026-03-15 16:55:55 +0100

    wip spaceship popup

--- projetpixel2/scripts/spaceship_info_window.gd
+++ projetpixel2/scripts/spaceship_info_window.gd
@@ -2,11 +2,24 @@ extends CasinoWindow
 class_name SpaceshipInfoWindow
 
 
-# Called when the node enters the scene tree for the first time.
+static var SPACESHIP_INFO_RES := load("res://scenes/interface/casino_minigames/spaceship_info_window.tscn")
+
+static func spawn_spaceship_info_popup() -> SpaceshipInfoWindow:
+	var popup : SpaceshipInfoWindow = SPACESHIP_INFO_RES.instantiate()
+	popup.position = random_popup_position()
+	spawn_popup(popup)
+	return popup
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	super._ready()
+	GV.space_ship.hit.connect(self._update_stats)
+	_update_stats()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _update_stats() -> void:
+	$Contents/LabelStats.text = \
+	"Health: " + str(GV.space_ship.get_health()) + \
+	"/" + str(GV.space_ship.get_max_health()) + \
+	"Shields: " + str(GV.space_ship.get_shields()) + \
+	"/" + str(GV.space_ship.get_max_shields())
+	
