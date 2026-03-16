eb876c807288760e40f6d4cdda885478aad6d9ca
Maxoups <mm.maxencemaire@gmail.com>
2026-03-16 14:04:24 +0100

    debug info popup

--- projetpixel2/scripts/info_popup.gd
+++ projetpixel2/scripts/info_popup.gd
@@ -16,7 +16,8 @@ static func add_popup(obj : Node) -> void:
 	var popup : Node
 	if obj in popups.keys():
 		popup = popups[obj]
-		popup.close_window()
+		if is_instance_valid(popup):
+			popup.close_window()
 	else:
 		if obj is TowerBase:
 			popup = TowerInfoWindow.spawn_tower_info_popup(obj)
