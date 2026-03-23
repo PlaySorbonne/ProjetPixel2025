5e491736c779a6eb5242d378b6ff25ab16946a96
Maxou <78730104+Maxoups@users.noreply.github.com>
2026-03-23 11:54:34 +0100

    Update loaded_dice.gd

--- projetpixel2/scenes/card_objects/cards_res/loaded_dice.gd
+++ projetpixel2/scenes/card_objects/cards_res/loaded_dice.gd
@@ -2,10 +2,9 @@ extends CardBase
 class_name LoadedDice
 
 
-@export var better_luck_duration := 5.0
-
 func check_condition():
 	return true
 
 func run_effect():
-	RunData.add_better_luck(better_luck_duration)
+	var dice_btn := GV.minigame_buttons[CasinoMinigameButton.Minigames.Dice]
+	dice_btn.min_reset_level
